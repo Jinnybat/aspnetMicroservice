446c186abf98afe5163e35e39effefd1ba92ac5f
Deepak <38551724+Jinnybat@users.noreply.github.com>
2023-05-11 06:36:57 +0000

    Adding testing template project to cart api

---
+++ src/Services/Cart/Cart.API.Tests/CartRepositoryTests.cs
@@ -0,0 +1,12 @@
+using Moq;
+
+namespace Cart.API.Tests;
+
+public class CartRepositoryTests
+{
+    [Fact]
+    public void Test_GetBasket()
+    {
+        //Assert.IsTrue("Deepak","Deepak");
+    }
+}
