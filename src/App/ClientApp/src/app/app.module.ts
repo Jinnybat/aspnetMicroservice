cff569f465660a54f0d69811d8c469aa513d87ff
Deepak Bisht <Deepak.x.Bisht1@GOINDIGO.IN>
2023-06-13 13:36:46 +0530

    changing hierarchy

--- src/App/ClientApp/src/app/app.module.ts
+++ src/App/ClientApp/src/app/app.module.ts
@@ -4,10 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout/layout.component';
-import { SideNavComponent } from './side-nav/side-nav.component';
-import { FooterComponent } from './footer/footer.component';
-import { ContentComponent } from './content/content.component';
-import { TopNavComponent } from './top-nav/top-nav.component';
+import { SideNavComponent } from './layout/side-nav/side-nav.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { ContentComponent } from './layout/content/content.component';
+import { TopNavComponent } from './layout/top-nav/top-nav.component';
 
 @NgModule({
   declarations: [
