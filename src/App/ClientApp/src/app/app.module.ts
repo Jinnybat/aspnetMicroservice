3abf49e51c5473624ff189e588b1372208888ad5
Deepak Bisht <Deepak.x.Bisht1@GOINDIGO.IN>
2023-06-13 12:53:19 +0530

    refactored layout

--- src/App/ClientApp/src/app/app.module.ts
+++ src/App/ClientApp/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout/layout.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    SideNavComponent,
+    FooterComponent,
+    ContentComponent,
+    TopNavComponent
   ],
   imports: [
     BrowserModule,
